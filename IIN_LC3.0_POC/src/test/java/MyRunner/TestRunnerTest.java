66c40cf2aeb15947c886655cd7f75693310793d3
Sneha Kumari <sneha.kumari@irissoftware.com>
2023-01-09 22:09:04 +0530

    Test Runner

--- IIN_LC3.0_POC/src/test/java/MyRunner/TestRunnerTest.java
+++ IIN_LC3.0_POC/src/test/java/MyRunner/TestRunnerTest.java
@@ -12,7 +12,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(stepNotifications=true,monochrome = true,dryRun=false,
 features =
-{"src/test/java/LC3_NewUserFeatures1/"},
+{"src/test/java/LC3_NewUserFeatures/"},
 plugin =  {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, 
 
 glue = {"stepDefinitions_LC_ExistingUser"})    
